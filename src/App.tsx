a94c0bbe94c4885af655b80f10ba7d5c83c1528b
Blacxsheepz <blacxsheepz@gmail.com>
2025-03-17 14:14:49 +0700

    change file name

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import TarotCardPicker from "./pages/TarotCardPage";
-import PhoneNumber from "./pages/PhoneNumber";
+import PhoneNumber from "./pages/PhoneNumberPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
